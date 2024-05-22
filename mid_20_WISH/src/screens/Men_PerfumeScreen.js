1a6f471ee78138d1c2fcdaf1f723603e908bea7d
Rupertrupert <133252503+Rupertrupert@users.noreply.github.com>
2024-05-22 23:44:05 +0800

    解決最下面兩張衣服沒辦法完全顯現的問題

--- mid_20_WISH/src/screens/Men_PerfumeScreen.js
+++ mid_20_WISH/src/screens/Men_PerfumeScreen.js
@@ -4,6 +4,7 @@ import { Box } from "@gluestack-ui/themed";
 import Men_PerfumeList from "../components/Men_PerfumeList";
 import MenPerfumeData from "../json/Men_Perfume.json";
 import { useNavigation } from '@react-navigation/native';
+import { FlatList } from 'react-native';
 
 const Men_PerfumeScreen = () => {
     const navigation = useNavigation();
@@ -21,11 +22,9 @@ const Men_PerfumeScreen = () => {
     }, [navigation]);
 
     return (
-        <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
-            <Box style={{ backgroundColor: "#fff", height: '100%' }}>
-                <Men_PerfumeList MenPerfumeList={MenPerfumeData} />
-            </Box>
-        </ScrollView>
+        <Box style={{ backgroundColor: "#fff", flex: 1, paddingBottom: 20 }}>
+            <Men_PerfumeList MenPerfumeList={MenPerfumeData} />
+        </Box>
     );
 };
 
